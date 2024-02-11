4c749c8b3af8499f5f6220bcc8f445057e474c2b
Yuna Tomida <ytomida.mmm@gmail.com>
2024-02-11 14:15:25 +0900

    feat: add minimum editor using blocknote

---
+++ src/app/components/Editor.tsx
@@ -0,0 +1,35 @@
+import { BlockNoteEditor } from "@blocknote/core";
+
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+
+import "@blocknote/core/style.css";
+import { useTheme } from "next-themes";
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
+}
+
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
+  const resolvedTheme = useTheme().resolvedTheme;
+
+  const editor: BlockNoteEditor = useBlockNote({
+    editable,
+    initialContent: initialContent ? JSON.parse(initialContent) : undefined,
+    onEditorContentChange(editor) {
+      onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
+    },
+  });
+
+  return (
+    <div>
+      <BlockNoteView
+        editor={editor}
+        theme={resolvedTheme === "dark" ? "dark" : "light"}
+      />
+    </div>
+  );
+};
+
+export default Editor;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Suggest from "./suggest";
+import Editor from "./components/Editor";
 
 const App = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-blue-300">
       <Suggest />
+      <Editor
+        onChange={(value) => {
+          // TODO: update document
+          console.log(value);
+        }}
+        initialContent="Hello Brainlight!"
+        editable={true}
+      />
     </main>
   );
 };
